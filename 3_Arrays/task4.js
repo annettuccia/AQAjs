bc8352a205a41ab477f515dddbfc48e67380bdef
Anna <tsed.anna@gmail.com>
2026-06-12 15:27:23 +0300

    fix: task4.js to a simpler version

--- 3_Arrays/task4.js
+++ 3_Arrays/task4.js
@@ -6,71 +6,123 @@ const seq2 = ['(', ')', '(', ')', '{', '(', '}', ')', 2, 'a'];
 const seq3 = ['(', ')', '(', '(', '(', ')', '}', '(', ')', ')'];
 
 // Проверка для первой последовательности
-let stack = [];
-let isValid = true;
+let openRound = 0;
+let openCurly = 0;
+let openSquare = 0;
+let isValid;
 
-for (let i = 0; i < seq1.length; i++) {
-    if (seq1[i] === '(' || seq1[i] === '{' || seq1[i] === '[') {
-        stack.push(seq1[i]);
-    } else if (seq1[i] === ')' || seq1[i] === '}' || seq1[i] === ']') {
-        if (stack.length === 0) {
-            isValid = false;
+for(let i = 0; i < seq1.length; i++){
+     switch(seq1[i]){
+        case '(':
+            openRound++;
             break;
-        }
-        let last = stack.pop();
-        if ((seq1[i] === ')' && last !== '(') ||
-            (seq1[i] === '}' && last !== '{') ||
-            (seq1[i] === ']' && last !== '[')) {
-            isValid = false;
+        case ')':
+            openRound--;
             break;
-        }
-    }
+        case '{':
+            openCurly++;
+            break;
+        case '}':
+            openCurly--;
+            break;
+        case '[':
+            openSquare++;
+            break;
+        case ']':
+            openSquare--;
+            break;
+        default:
+            break;
+     }
 }
-if (stack.length !== 0) isValid = false;
+
+if(openRound === 0 && openCurly === 0 && openSquare === 0){
+    isValid = true;
+}
+else{
+    isValid = false
+}
+
 console.log("Первая последовательность:", isValid);
 
-// Для второй последовательности
-stack = [];
-isValid = true;
-for (let i = 0; i < seq2.length; i++) {
-    if (seq2[i] === '(' || seq2[i] === '{' || seq2[i] === '[') {
-        stack.push(seq2[i]);
-    } else if (seq2[i] === ')' || seq2[i] === '}' || seq2[i] === ']') {
-        if (stack.length === 0) {
-            isValid = false;
+// Проверка для второй последовательности
+openRound = 0;
+openCurly = 0;
+openSquare = 0;
+isValid;
+
+
+for(let i = 0; i < seq2.length; i++){
+     switch(seq2[i]){
+        case '(':
+            openRound++;
             break;
-        }
-        let last = stack.pop();
-        if ((seq2[i] === ')' && last !== '(') ||
-            (seq2[i] === '}' && last !== '{') ||
-            (seq2[i] === ']' && last !== '[')) {
-            isValid = false;
+        case ')':
+            openRound--;
             break;
-        }
-    }
+        case '{':
+            openCurly++;
+            break;
+        case '}':
+            openCurly--;
+            break;
+        case '[':
+            openSquare++;
+            break;
+        case ']':
+            openSquare--;
+            break;
+        default:
+            break;
+     }
 }
-if (stack.length !== 0) isValid = false;
+
+if(openRound === 0 && openCurly === 0 && openSquare === 0){
+    isValid = true;
+}
+else{
+    isValid = false
+}
+
 console.log("Вторая последовательность:", isValid);
 
-// Для третьей последовательности
-stack = [];
-isValid = true;
-for (let i = 0; i < seq3.length; i++) {
-    if (seq3[i] === '(' || seq3[i] === '{' || seq3[i] === '[') {
-        stack.push(seq3[i]);
-    } else if (seq3[i] === ')' || seq3[i] === '}' || seq3[i] === ']') {
-        if (stack.length === 0) {
-            isValid = false;
+// Проверка для третьей последовательности
+openRound = 0;
+openCurly = 0;
+openSquare = 0;
+isValid;
+
+
+for(let i = 0; i < seq3.length; i++){
+     switch(seq3[i]){
+        case '(':
+            openRound++;
             break;
-        }
-        let last = stack.pop();
-        if ((seq3[i] === ')' && last !== '(') ||
-            (seq3[i] === '}' && last !== '{') ||
-            (seq3[i] === ']' && last !== '[')) {
-            isValid = false;
+        case ')':
+            openRound--;
             break;
-        }
-    }
+        case '{':
+            openCurly++;
+            break;
+        case '}':
+            openCurly--;
+            break;
+        case '[':
+            openSquare++;
+            break;
+        case ']':
+            openSquare--;
+            break;
+        default:
+            break;
+     }
 }
-if (stack.length !== 0) isValid = false;
+
+if(openRound === 0 && openCurly === 0 && openSquare === 0){
+    isValid = true;
+}
+else{
+    isValid = false
+}
+
 console.log("Третья последовательность:", isValid);
